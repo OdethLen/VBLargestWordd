92e690cc83c8ea08dbb24575c3cc2005f0817841
Odeth Ayellén Peña Gómez <odethayellen@hotmail.com>
2024-05-21 17:02:17 -0600

    Agregar archivos de proyecto.

---
+++ VBLargestWordd/Form1.Designer.vb
@@ -0,0 +1,76 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblLargestWord = New Label()
+        txtDocument = New TextBox()
+        btnOpen = New Button()
+        SuspendLayout()
+        ' 
+        ' lblLargestWord
+        ' 
+        lblLargestWord.AutoSize = True
+        lblLargestWord.Location = New Point(243, 227)
+        lblLargestWord.Name = "lblLargestWord"
+        lblLargestWord.Size = New Size(16, 15)
+        lblLargestWord.TabIndex = 5
+        lblLargestWord.Text = "..."
+        ' 
+        ' txtDocument
+        ' 
+        txtDocument.Location = New Point(112, 67)
+        txtDocument.Multiline = True
+        txtDocument.Name = "txtDocument"
+        txtDocument.ReadOnly = True
+        txtDocument.ScrollBars = ScrollBars.Vertical
+        txtDocument.Size = New Size(300, 139)
+        txtDocument.TabIndex = 4
+        ' 
+        ' btnOpen
+        ' 
+        btnOpen.Location = New Point(217, 25)
+        btnOpen.Name = "btnOpen"
+        btnOpen.Size = New Size(75, 23)
+        btnOpen.TabIndex = 3
+        btnOpen.Text = "Open"
+        btnOpen.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(555, 263)
+        Controls.Add(lblLargestWord)
+        Controls.Add(txtDocument)
+        Controls.Add(btnOpen)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblLargestWord As Label
+    Friend WithEvents txtDocument As TextBox
+    Friend WithEvents btnOpen As Button
+
+End Class
---
+++ VBLargestWordd/Form1.vb
@@ -0,0 +1,39 @@
+﻿
+Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Wordprocessing
+Imports System.IO
+Public Class Form1
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        Dim openfiledialog As New OpenFileDialog()
+        openfiledialog.Filter = "DOCX FILE |*.docx"
+
+        If openfiledialog.ShowDialog() = DialogResult.OK Then
+            Using WordDocument As WordprocessingDocument = WordprocessingDocument.Open(openfiledialog.FileName, False)
+                Dim body As New Body
+                body = WordDocument.MainDocumentPart.Document.Body
+                Dim text As String = ""
+
+                For Each paragraph As Paragraph In body.Elements(Of Paragraph)()
+                    For Each run As Run In paragraph.Elements(Of Run)()
+                        For Each textElement As Text In run.Elements(Of Text)()
+                            text += textElement.Text
+                        Next
+                    Next
+                Next
+
+                txtDocument.Text = text
+
+                Dim Words() As String = text.Split({" ", vbTab, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+                Dim LargestWord As String = ""
+                For Each Word As String In Words
+                    If Word.Length > LargestWord.Length Then
+                        LargestWord = Word
+                    End If
+                Next
+                lblLargestWord.Text = "Largest Word: " + LargestWord
+
+            End Using
+        End If
+
+    End Sub
+End Class
